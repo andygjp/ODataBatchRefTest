6b1766632cedd596277886718afd7d1aab026918
Andrew Pattison <andygjp@hotmail,com>
2021-11-08 16:46:57 +0000

    initial commit

---
+++ ODataBatchRefTest/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.OData.Batch;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.OData.ModelBuilder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddControllers()
+    .AddOData(options =>
+    {
+        var modelBuilder = new ODataConventionModelBuilder();
+        modelBuilder.EntitySet<Contact>("Contacts");
+        options.AddRouteComponents(modelBuilder.GetEdmModel(), new DefaultODataBatchHandler());
+    });
+
+var app = builder.Build();
+
+app.UseODataBatching().UseRouting().UseEndpoints(routeBuilder => routeBuilder.MapControllers());
+
+app.Run();
+
+public class Contact
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
+
+[EnableQuery]
+public class Contacts : ODataController
+{
+    public IQueryable<Contact> Get()
+    {
+        return new[] { new Contact() }.AsQueryable();
+    }
+    
+    public Contact Get(int key)
+    {
+        return new Contact { Id = key };
+    }
+    
+    public Contact Post(Contact entity)
+    {
+        return entity;
+    }
+}
